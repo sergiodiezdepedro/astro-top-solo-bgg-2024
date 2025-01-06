98afdffcf649f36738f7960e6ecd756680775ec6
sergiodiezdepedro <sergio.d.depedro@gmail.com>
2025-01-06 15:31:15 +0100

    First commit.Starting adding data

---
+++ src/data/articles.js
@@ -0,0 +1,94 @@
+export const articles150 = [
+  {
+    position: "1",
+    iconVariation: "=",
+    variation: "",
+    title: "Spirit Island",
+    url: "https://boardgamegeek.com/boardgame/162886/spirit-island",
+  },
+  {
+    position: "2",
+    iconVariation: "=",
+    variation: "",
+    title: "Mage Knight: Ultimate Edition",
+    url: "https://boardgamegeek.com/boardgame/248562/mage-knight-ultimate-edition",
+  },
+  {
+    position: "3",
+    iconVariation: "=",
+    variation: "",
+    title: "Marvel Champions: The Card Game",
+    url: "https://boardgamegeek.com/boardgame/285774/marvel-champions-the-card-game",
+  },
+  {
+    position: "4",
+    iconVariation: "=",
+    variation: "",
+    title: "Arkham Horror: The Card Game",
+    url: "https://boardgamegeek.com/boardgame/205637/arkham-horror-the-card-game",
+  },
+  {
+    position: "5",
+    iconVariation: "▲",
+    variation: "1",
+    title: "Too Many Bones",
+    url: "https://boardgamegeek.com/boardgame/192135/too-many-bones",
+  },
+  {
+    position: "6",
+    iconVariation: "▼",
+    variation: "1",
+    title: "The Lord of the Rings: The Card Game",
+    url: "https://boardgamegeek.com/boardgame/77423/the-lord-of-the-rings-the-card-game",
+  },
+];
+
+export const articles51100 = [
+  {
+    position: "51",
+    iconVariation: "=",
+    variation: "",
+    title: "Spirit Island",
+    url: "https://boardgamegeek.com/boardgame/162886/spirit-island",
+  },
+];
+
+export const articles101150 = [
+  {
+    position: "101",
+    iconVariation: "=",
+    variation: "",
+    title: "Spirit Island",
+    url: "https://boardgamegeek.com/boardgame/162886/spirit-island",
+  },
+];
+
+export const articles151200 = [
+  {
+    position: "151",
+    iconVariation: "=",
+    variation: "",
+    title: "Spirit Island",
+    url: "https://boardgamegeek.com/boardgame/162886/spirit-island",
+  },
+];
+
+export const articles201250 = [
+  {
+    position: "201",
+    iconVariation: "=",
+    variation: "",
+    title: "Spirit Island",
+    url: "https://boardgamegeek.com/boardgame/162886/spirit-island",
+  },
+];
+
+export const articles251300 = [
+  {
+    position: "251",
+    iconVariation: "=",
+    variation: "",
+    title: "Spirit Island",
+    url: "https://boardgamegeek.com/boardgame/162886/spirit-island",
+  },
+];
